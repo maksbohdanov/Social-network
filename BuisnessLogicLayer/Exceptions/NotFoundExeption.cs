5ecb93ca151f1a9778d8799d267185cfd52848e4
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2022-11-22 11:45:45 +0200

    Create custom exceptions

---
+++ BuisnessLogicLayer/Exceptions/NotFoundExeption.cs
@@ -0,0 +1,9 @@
+﻿namespace BuisnessLogicLayer.Exceptions
+{
+    public class NotFoundException : SocialNetworkException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ BuisnessLogicLayer/Exceptions/SocialNetworkException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace BuisnessLogicLayer.Exceptions
+{
+    [Serializable]
+    public class SocialNetworkException: Exception
+    {
+        public SocialNetworkException() 
+        {
+        }
+
+        public SocialNetworkException(string message) : base(message)
+        {
+        }
+
+        public SocialNetworkException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected SocialNetworkException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
