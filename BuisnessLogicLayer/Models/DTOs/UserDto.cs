0e1cc1bf46252da17cf38f4338f36bbf5d3a079a
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2022-11-26 19:18:16 +0200

    Create login and registration models

--- BuisnessLogicLayer/Models/DTOs/UserDto.cs
+++ BuisnessLogicLayer/Models/DTOs/UserDto.cs
@@ -4,7 +4,7 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string UserName { get; set; }
+        public string Email { get; set; }
         public string City { get; set; }
         public DateTime BirthDate { get; set; }
         public DateTime RegistrationDate { get; set; }
---
+++ BuisnessLogicLayer/Models/UserRegistrationModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BuisnessLogicLayer.Models
+{
+    public class UserRegistrationModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
