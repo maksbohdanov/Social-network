f964133a5a335f2485c6e7ab9dec3bfd904eaf95
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2022-11-27 13:10:03 +0200

    Configure error handling middleware

---
+++ WebAPI/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using BuisnessLogicLayer.Exceptions;
+using WebAPI.Models;
+
+namespace WebAPI.Middlewares
+{
+    public class ErrorHandlerMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.ContentType = "application/json";
+                var error = new ErrorDetails() { Message = ex.Message };
+
+                switch (ex)
+                {
+                    case NotFoundException:
+                        context.Response.StatusCode = StatusCodes.Status404NotFound;
+                        break;
+
+                    case SocialNetworkException:
+                        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                        break;
+
+                    default:
+                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                        error.Message = "Internal error occured. Please try later.";
+                        break;
+                }
+
+                error.Status = context.Response.StatusCode.ToString();                
+
+                await context.Response.WriteAsync(error.ToString());
+            }
+        }
+
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
+using WebAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +26,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<ErrorHandlerMiddleware>();
 
 app.MapControllers();
 
