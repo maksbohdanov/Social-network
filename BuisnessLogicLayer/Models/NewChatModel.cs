f02f3ec3a3f35a44805ce57e3a08455961efb19d
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2022-11-28 22:42:55 +0200

    Add Chats controller

---
+++ BuisnessLogicLayer/Models/NewChatModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BuisnessLogicLayer.Models
+{
+    public class NewChatModel
+    {
+        public string FirstUserId { get; set; }
+        public string SecondUserId { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/ChatsController.cs
@@ -0,0 +1,56 @@
+﻿using BuisnessLogicLayer.Interfaces;
+using BuisnessLogicLayer.Models;
+using BuisnessLogicLayer.Models.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChatsController : ControllerBase
+    {
+        private readonly IChatService _chatService;
+        public ChatsController(IChatService chatService)
+        {
+            _chatService= chatService;
+        }
+
+        [HttpGet("users/{userId}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<ChatDto>))]
+        public async Task<IActionResult> GetUserChats(string userId)
+        {
+            var chats = await _chatService.GetAllAsync(userId);
+            return Ok(chats);
+        }
+
+        [HttpGet("{chatId}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ChatDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorDetails))]
+        public async Task<IActionResult> GetById(string chatId)
+        {
+            var chat = await _chatService.GetByIdAsync(chatId);
+            return Ok(chat);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ChatDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ErrorDetails))]
+        public async Task<IActionResult> CreateChat([FromBody] NewChatModel newChat)
+        {
+            var createdChat = await _chatService.CreateChatAsync(newChat);
+            return CreatedAtAction(nameof(GetById), new { id = createdChat.Id }, createdChat);
+        }
+
+
+        [HttpDelete("{chatId}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorDetails))]
+        public async Task<IActionResult> DeleteById(string chatId)
+        {
+            var result = await _chatService.DeleteChatAsync(chatId);
+            return Ok(result);
+        }
+    }
+}
