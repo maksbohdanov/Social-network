1e92b92d9aee7b665768a206b2ca623e68db4a02
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2022-12-05 17:16:22 +0200

    Made repository methods virtual

--- DataAccessLayer/Repositories/ChatRepository.cs
+++ DataAccessLayer/Repositories/ChatRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Repositories
 {
@@ -6,6 +7,18 @@ namespace DataAccessLayer.Repositories
     {
         public ChatRepository(SocialNetworkDbContext context) : base(context)
         {
+        }
+        public override async Task<Chat> GetByIdAsync(string id)
+        {
+            return (await GetAllAsync())
+                .FirstOrDefault(e => e.Id.ToString() == id);
+        }
+        public override async Task<IEnumerable<Chat>> GetAllAsync()
+        {
+            return await _context.Chats
+                .Include(c => c.Users).AsNoTracking()
+                .Include(c => c.Messages).AsNoTracking()
+                .ToListAsync();
         }
     }
 }
--- DataAccessLayer/Repositories/Repository.cs
+++ DataAccessLayer/Repositories/Repository.cs
@@ -12,43 +12,44 @@ namespace DataAccessLayer.Repositories
             _context= context;
         }
 
-        public async Task<TEntity> GetByIdAsync(string id)
+        public virtual async Task<TEntity> GetByIdAsync(string id)
         {
             return (await GetAllAsync())
                 .FirstOrDefault(e => e.Id.ToString() == id);
         }
 
-        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
         {
             return await _context
                 .Set<TEntity>()
                 .ToListAsync();
         }
 
-        public async Task<IEnumerable<TEntity>> FindAsync(Func<TEntity, bool> predicate)
+        public virtual async Task<IEnumerable<TEntity>> FindAsync(Func<TEntity, bool> predicate)
         {
             return (await GetAllAsync())
                 .Where(predicate);
         }
 
-        public async Task<bool> UpdateAsync(TEntity entity)
+        public virtual async Task<bool> UpdateAsync(TEntity entity)
         {
             var dbEntity = await GetByIdAsync(entity.Id.ToString());
             if (dbEntity == null)
             {
                 return false;
             }
+            _context.Entry(dbEntity).State = EntityState.Detached;
             _context.Update(entity);
             return true;
         }
 
-        public async Task<bool> CreateAsync(TEntity entity)
+        public virtual async Task<bool> CreateAsync(TEntity entity)
         {
             await _context.AddAsync(entity);
             return true;
         }
 
-        public async Task<bool> DeleteByIdAsync(string id)
+        public virtual async Task<bool> DeleteByIdAsync(string id)
         {
             var entity = await GetByIdAsync(id);
             if (entity == null)
